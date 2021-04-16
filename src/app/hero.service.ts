20233b6aaea410a3fe74e58facaf0e355d32ae2f
sitek94 <msitkowski94@gmail.com>
2021-04-16 19:49:03 +0200

    feat(messages): add message component and service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,16 +2,18 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
+import { MessageService } from './message.service';
 import { HEROES } from './mock-heroes';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 }
